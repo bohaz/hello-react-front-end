bb346c1ae4b4a022b81ee50890972c8b1c9b38f5
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-31 16:50:31 -0300

    Add store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import greetingReducer from './reducers/greetingReducer';
+
+const store = createStore(
+  greetingReducer,
+  applyMiddleware(thunk)
+);
+
+export default store;
